c50c87aa359c9e6ba21393cf1e05a0f1991daddd
Bruce0203 <BruceFreedy@gmail.com>
2022-08-22 23:08:17 +0900

    add: publish to instargram

--- src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
+++ src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
@@ -1,13 +1,25 @@
 package io.github.bruce0203.bsmeal1nfo
 
+import com.github.instagram4j.instagram4j.IGClient
+import com.github.instagram4j.instagram4j.responses.media.MediaResponse.MediaConfigureTimelineResponse
 import kr.go.neis.api.School
 import java.io.File
+import java.text.SimpleDateFormat
 import java.util.*
 
 
 fun main() {
-    AddTextToImg.execute(File("assets/image.png"), getMyLunch(), File("output/dist.png"))
-    println(getMyLunch())
+    val dist = File("output/dist.png")
+    AddTextToImg.execute(File("assets/image.png"), getMyLunch(), dist)
+    val caption = SimpleDateFormat("yyyy.MM.dd(${getWeek()})").format(Date())
+    publish(dist, caption)
+}
+
+fun getWeek(): String {
+    val cal = Calendar.getInstance()
+    cal.time = Date()
+    println(cal[Calendar.DAY_OF_WEEK])
+    return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.NARROW_FORMAT, Locale.KOREAN)
 }
 
 fun getMyLunch(): String {
@@ -26,4 +38,19 @@ fun removeNumbersInString(input: String): String {
                 .indexOfFirst { Character.isDigit(it) || it == '(' }).run { if (this <= 0) txt.length else this }) }
         .forEach { strBuilder.append(it) ; strBuilder.append("\n")}
     return strBuilder.toString()
+}
+
+fun publish(dist: File, caption: String) {
+    val client = IGClient.builder()
+        .username(System.getenv("INSTARGRAM_USERNAME"))
+        .password(System.getenv("INSTARGRAM_PASSWORD"))
+        .login()
+    client.actions()
+        .timeline()
+        .uploadPhoto(dist, caption)
+        .thenAccept { response: MediaConfigureTimelineResponse? ->
+            println("Successfully uploaded photo!")
+            println(response)
+        }
+        .join() // block current thread until complete
 }
