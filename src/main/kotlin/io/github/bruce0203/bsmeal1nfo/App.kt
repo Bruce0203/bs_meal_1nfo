f42a96899f4d7a6d7d46111064727c6ef64a9e56
Bruce0203 <BruceFreedy@gmail.com>
2022-08-22 22:08:04 +0900

    test: log current date

--- src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
+++ src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
@@ -1,5 +1,15 @@
 package io.github.bruce0203.bsmeal1nfo
 
-fun main(args : Array<String>) {
-    println("Hello World")
+import kr.go.neis.api.School
+import java.util.*
+
+
+fun main() {
+    val school = School.find(School.Region.GYEONGGI, "백신고등학교")
+    val cal = Calendar.getInstance()
+    cal.time = Date()
+    println(cal)
+    val menu = school.getMonthlyMenu(cal[Calendar.YEAR], cal[Calendar.MONTH])
+    println(menu[cal[Calendar.DATE] - 1].lunch)
+
 }
