21d45ca0fa0b45083de1cfabb7bad5e93615c83f
Bruce0203 <BruceFreedy@gmail.com>
2022-08-23 23:10:59 +0900

    Get Lunch data, Draw Photo, upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,13 @@ repositories {
 }
 
 dependencies {
+    api("org.slf4j:slf4j-api:1.7.36")
+    api("org.slf4j:slf4j-simple:1.7.36")
     implementation("com.github.instagram4j:instagram4j:2.0.7")
     //https://github.com/agemor/neis-api/releases/tag/4.0.1
     api(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     api("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+    api("de.taimos:totp:1.0")
+    api("commons-codec:commons-codec:1.10")
+    api("com.google.zxing:javase:3.2.1")
 }
--- src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
+++ src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
@@ -1,5 +1,91 @@
 package io.github.bruce0203.bsmeal1nfo
 
-fun main(args : Array<String>) {
-    println("Hello World")
+import com.github.instagram4j.instagram4j.IGClient
+import com.github.instagram4j.instagram4j.IGClient.Builder.LoginHandler
+import com.github.instagram4j.instagram4j.responses.accounts.LoginResponse
+import com.github.instagram4j.instagram4j.responses.media.MediaResponse.MediaConfigureTimelineResponse
+import com.github.instagram4j.instagram4j.utils.IGChallengeUtils
+import de.taimos.totp.TOTP
+import kr.go.neis.api.School
+import org.apache.commons.codec.binary.Base32
+import org.apache.commons.codec.binary.Hex
+import java.awt.Color
+import java.awt.image.BufferedImage
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Callable
+import javax.imageio.ImageIO
+
+
+fun main() {
+    val png = File("output/dist.png")
+    AddTextToImg.execute(File("assets/image/image.png"), getMyLunch(), png)
+    val jpg = File("output/dist.jpg")
+    pngToJpg(png, jpg)
+    val caption = SimpleDateFormat("yyyy.MM.dd(${getWeek()})").format(Date())
+    publish(jpg, caption)
 }
+
+fun getWeek(): String {
+    val cal = Calendar.getInstance()
+    cal.time = Date()
+    return cal.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.NARROW_FORMAT, Locale.KOREAN)
+}
+
+fun getMyLunch(): String {
+    val school = School.find(School.Region.GYEONGGI, "백신고등학교")
+    val cal = Calendar.getInstance()
+    cal.time = Date()
+    val menu = school.getMonthlyMenu(cal[Calendar.YEAR], cal[Calendar.MONTH] + 1)
+    return menu[cal[Calendar.DATE] - 1].lunch.run(::removeNumbersInString)
+}
+
+fun removeNumbersInString(input: String): String {
+    val strBuilder = StringBuilder()
+    input.split("\n")
+        .map { txt -> txt.substring(0,
+            0.coerceAtLeast(txt
+                .indexOfFirst { Character.isDigit(it) || it == '(' }).run { if (this <= 0) txt.length else this }) }
+        .forEach { strBuilder.append(it) ; strBuilder.append("\n")}
+    return strBuilder.toString()
+}
+
+fun publish(dist: File, caption: String) {
+
+    val inputCode = Callable { getTOTPCode(System.getenv("OTP_SECRET")) }
+    val twoFactorHandler = LoginHandler { client: IGClient, response: LoginResponse ->
+        IGChallengeUtils.resolveTwoFactor(client, response, inputCode)
+    }
+
+    val client = IGClient.builder()
+        .username(System.getenv("INSTARGRAM_USERNAME"))
+        .password(System.getenv("INSTARGRAM_PASSWORD"))
+        .onTwoFactor(twoFactorHandler)
+        .login()
+    client.actions()
+        .timeline()
+        .uploadPhoto(dist, caption)
+        .thenAccept { response: MediaConfigureTimelineResponse? ->
+            println(
+                "Successfully uploaded photo!"
+            )
+        }
+        .join() // block current thread until complete
+
+}
+
+fun getTOTPCode(secretKey: String?): String? {
+    val base32 = Base32()
+    val bytes: ByteArray = base32.decode(secretKey)
+    val hexKey: String = Hex.encodeHexString(bytes)
+    return TOTP.getOTP(hexKey)
+}
+
+fun pngToJpg(png: File, jpg: File) {
+    val beforeImg = ImageIO.read(png)
+    val afterImg = BufferedImage(beforeImg.width, beforeImg.height, BufferedImage.TYPE_INT_RGB)
+    afterImg.createGraphics().drawImage(beforeImg, 0, 0, Color.white, null)
+    ImageIO.write(afterImg, "jpg", jpg)
+}
+
