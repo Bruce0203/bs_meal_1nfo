23e26f9fcbef41410a31c15a7e94f9f7ad3a2ba1
Bruce0203 <BruceFreedy@gmail.com>
2023-06-07 12:58:23 +0900

    fix: unnecessary code removed

--- src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
+++ src/main/kotlin/io/github/bruce0203/bsmeal1nfo/App.kt
@@ -1,41 +1,14 @@
 package io.github.bruce0203.bsmeal1nfo
 
-import java.util.*
 import kotlin.system.exitProcess
 
-var isRunCold = false
-
 fun main(args: Array<String>) {
-    isRunCold = args.getOrElse(0) { "false" }.toBoolean()
     publish()
 }
 
 fun publish() {
     val lunch = getMyLunch()
     val client = login()
-    val cal = Calendar.getInstance(TimeZone.getTimeZone("Asia/Seoul"))
-    cal.time = Date()
-    val timeZoneOffset = (cal.timeZone.getOffset(cal.timeInMillis) / 1000.0).toLong()
-    val sec = (cal.timeInMillis / 1000.0).toInt() + timeZoneOffset
-    val takenAt = (client.actions.timeline().feed().maxOfOrNull { feed -> feed.feed_items.maxOfOrNull { it.taken_at } ?: 0L }?: 0) + timeZoneOffset
-    val dayStart = sec - (sec % 86400)
-    println("""
-    ---------debugMsg---------
-    --------------------------
-    "takenAt: $takenAt"
-    "timeZoneOffset: $timeZoneOffset"
-    "dayStart: $dayStart"
-    --------------------------
-    """)
-    if (dayStart < takenAt && !isRunCold) {
-            println(
-                """
-                    --------------------------
-                   "Skipped due to already exist!" 
-                    --------------------------
-                """
-            )
-    }
     client.actions()
         .story()
         .uploadPhoto(createImg(lunch))
@@ -45,7 +18,7 @@ fun publish() {
                     --------------------------
                    "Successfully uploaded photo!" 
                     --------------------------
-                """
+                """.trimIndent()
             )
         }
         .join() // block current thread until complete
